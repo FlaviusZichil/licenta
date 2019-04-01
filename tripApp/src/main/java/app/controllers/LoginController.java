9964e6af4e79d3ad54287ba4c5e01ca06715626e
FlaviusZichil <Flazichi_07@yahoo.com>
2019-04-01 20:12:00 +0300

    deleted some useless imports

--- tripApp/src/main/java/app/controllers/LoginController.java
+++ tripApp/src/main/java/app/controllers/LoginController.java
@@ -1,19 +1,7 @@
 package app.controllers;
 
-import java.security.Principal;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.util.WebUtils;
 
 @Controller
 public class LoginController {
--- tripApp/src/main/java/app/dto/PeakDTO.java
+++ tripApp/src/main/java/app/dto/PeakDTO.java
@@ -1,18 +1,6 @@
 package app.dto;
 
 import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
-import app.entities.Mountain;
-import app.entities.Peak;
 import app.entities.Trip;
 
 public class PeakDTO {

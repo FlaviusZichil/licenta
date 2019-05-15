d607b2cdb96e46d584584fcb74e277cec213c34c
FlaviusZichil <Flazichi_07@yahoo.com>
2019-05-15 11:26:16 +0300

    display medals for user

--- tripApp/src/main/java/app/controllers/AchievementsController.java
+++ tripApp/src/main/java/app/controllers/AchievementsController.java
@@ -1,5 +1,6 @@
 package app.controllers;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import app.dto.MedalDTO;
 import app.entities.Medal;
+import app.entities.UserEntity;
+import app.entities.UserMedal;
 import app.models.AllMedalsViewModel;
 import app.repositories.MedalRepository;
 import app.utils.TripUtils;
+import app.utils.UserUtils;
 
 @Controller
 public class AchievementsController {
@@ -21,11 +25,21 @@ public class AchievementsController {
 	@Autowired
 	private MedalRepository medalRepository;
 	
+	@Autowired
+	private UserUtils userUtils;
+	
 	@GetMapping("/achievements")
-	public String getAchievements(Model model, AllMedalsViewModel allMedalViewModel) {
-		List<MedalDTO> medalsDTO = this.getAllMedalsDTO();
-		allMedalViewModel.setMedalsDTO(medalsDTO);
+	public String getAchievements(Model model, AllMedalsViewModel allMedalViewModel, Principal principal) {
+//		List<MedalDTO> medalsDTO = this.getAllMedalsDTO();
+			
+		UserEntity user = userUtils.getUserByEmail(principal.getName());
+		allMedalViewModel.setMedalsDTO(this.getAllMedalsForUser(user));
 		model.addAttribute("allMedalViewModel", allMedalViewModel);
+		
+		for(MedalDTO medal : this.getAllMedalsForUser(user)) {
+			System.out.println(medal.toString());
+		}
+		
 		return "views/all/achievementsView";
 	}
 	
@@ -45,5 +59,18 @@ public class AchievementsController {
 	private MedalDTO convertFromMedalToMedalDTO(Medal medal) {
 		MedalDTO medalDTO = new MedalDTO(medal.getMedal_id(), TripUtils.convertFromPeakToPeakDTO(medal.getPeak()));
 		return medalDTO;
+	}
+	
+	private List<MedalDTO> getAllMedalsForUser(UserEntity user){
+		List<MedalDTO> medalsForUser = this.getAllMedalsDTO();
+		
+		for(MedalDTO medalDTO : medalsForUser) {
+			for(UserMedal userMedal : user.getMedals()) {
+				if(userMedal.getMedal().getMedal_id() == medalDTO.getMedal_id()) {
+					medalDTO.setOwned(true);
+				}
+			}
+		}
+		return medalsForUser;
 	}
 }
--- tripApp/src/main/java/app/dto/MedalDTO.java
+++ tripApp/src/main/java/app/dto/MedalDTO.java
@@ -7,6 +7,7 @@ public class MedalDTO {
 
 	private Integer medalId;
     private PeakDTO peakDTO;
+    private boolean owned;
     private List<UserMedal> userMedals;
     
     public MedalDTO() {}
@@ -15,10 +16,28 @@ public class MedalDTO {
 		super();
 		this.medalId = medal_id;
 		this.peakDTO = peakDTO;
+		this.owned = false;
 	}
+	
 	public Integer getMedal_id() {
 		return medalId;
 	}
+	public Integer getMedalId() {
+		return medalId;
+	}
+
+	public void setMedalId(Integer medalId) {
+		this.medalId = medalId;
+	}
+
+	public boolean isOwned() {
+		return owned;
+	}
+
+	public void setOwned(boolean owned) {
+		this.owned = owned;
+	}
+
 	public void setMedal_id(Integer medal_id) {
 		this.medalId = medal_id;
 	}
@@ -34,8 +53,9 @@ public class MedalDTO {
 	public void setUserMedals(List<UserMedal> userMedals) {
 		this.userMedals = userMedals;
 	}
+
 	@Override
 	public String toString() {
-		return "MedalDTO [medal_id=" + medalId + ", peakDTO=" + peakDTO + ", userMedals=" + userMedals + "]";
+		return "MedalDTO [medalId=" + medalId + ", peakDTO=" + peakDTO + ", owned=" + owned + "]";
 	}
 }

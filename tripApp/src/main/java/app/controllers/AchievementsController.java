871f8e6a60c242acec672ce2fd026f84d9edc6b4
FlaviusZichil <Flazichi_07@yahoo.com>
2019-05-15 20:27:53 +0300

    added some more func on medals part

--- tripApp/src/main/java/app/controllers/AchievementsController.java
+++ tripApp/src/main/java/app/controllers/AchievementsController.java
@@ -3,20 +3,28 @@ package app.controllers;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import app.dto.MedalDTO;
+import app.dto.TripDTO;
 import app.entities.Medal;
 import app.entities.Trip;
 import app.entities.UserEntity;
 import app.entities.UserMedal;
 import app.models.AllMedalsViewModel;
+import app.models.TripViewModel;
 import app.repositories.MedalRepository;
+import app.repositories.TripRepository;
 import app.utils.TripUtils;
 import app.utils.UserUtils;
 
@@ -27,10 +35,13 @@ public class AchievementsController {
 	private MedalRepository medalRepository;
 	
 	@Autowired
+	private TripRepository tripRepository;
+	
+	@Autowired
 	private UserUtils userUtils;
 	
 	@GetMapping("/achievements")
-	public String getAchievements(Model model, AllMedalsViewModel allMedalViewModel, Principal principal) {			
+	public String getAchievements(Model model, AllMedalsViewModel allMedalViewModel, Principal principal, HttpSession session) {			
 		UserEntity user = userUtils.getUserByEmail(principal.getName());
 		allMedalViewModel.setMedalsDTO(this.getAllMedalsForUser(user));
 		model.addAttribute("allMedalViewModel", allMedalViewModel);
@@ -44,12 +55,50 @@ public class AchievementsController {
 		Integer numberOfFinishedTrips = this.getNumberOFAllFinishedTripsForUser(user);
 		model.addAttribute("numberOfFinishedTrips", numberOfFinishedTrips);
 		
+		if(session.getAttribute("noAvailableTripsForThatMedal") != null) {
+			if((boolean)session.getAttribute("noAvailableTripsForThatMedal")) {
+				model.addAttribute("noAvailableTripsForThatMedal", true);
+				session.setAttribute("noAvailableTripsForThatMedal", false);
+			}
+		}	
 		return "views/all/achievementsView";
 	}
 	
 	@PostMapping("/achievements")
-	public String achievementsActions() {
+	public String achievementsActions(Model model, TripViewModel tripViewModel, HttpSession session, Principal principal,
+									  @RequestParam MultiValueMap<String, String> selectedMedal) {
+		UserEntity user = userUtils.getUserByEmail(principal.getName());
+
+		if(selectedMedal != null) {
+			for(Map.Entry<String, List<String>> entry : selectedMedal.entrySet()){
+				if(entry.getValue().contains("Obtine medalia")){
+					List<TripDTO> tripsDTOForUser = new ArrayList<>();
+					for(Trip trip : tripRepository.findAll()) {
+						if(trip.getPeak().getId() == Integer.parseInt(entry.getKey()) && !this.isUserRegisteredForTrip(user, trip)) {
+							tripsDTOForUser.add(TripUtils.convertFromTripToTripDTO(trip));
+						}
+					}
+					if(tripsDTOForUser.size() == 0) {
+						session.setAttribute("noAvailableTripsForThatMedal", true);
+						return "redirect:/achievements";
+					}
+					tripViewModel.setTripsDTO(tripsDTOForUser);
+					model.addAttribute("tripViewModel", tripViewModel);	
+					return "views/all/allTrips";
+				}
+			}
+		}
+		System.out.println("a intrat aici3");
 		return "views/all/achievementsView";
+	}
+	
+	private boolean isUserRegisteredForTrip(UserEntity user, Trip trip) {
+		for (Trip tripObj : user.getTrips()) {
+			if (tripObj.getId() == trip.getId()) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	private List<MedalDTO> getAllMedalsDTO(){

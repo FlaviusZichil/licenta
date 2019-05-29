da9ef641d22ac7302aa9c15a20a7de310b215999
FlaviusZichil <Flazichi_07@yahoo.com>
2019-05-29 17:35:51 +0300

    added change password func

--- tripApp/src/main/java/app/controllers/PersonalDataController.java
+++ tripApp/src/main/java/app/controllers/PersonalDataController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import app.entities.UserEntity;
 import app.models.PersonalDataViewModel;
-import app.utils.Conversion;
+import app.repositories.UserRepository;
 import app.utils.UserUtils;
+import app.validators.RegisterValidator;
 
 @Controller
 public class PersonalDataController {
@@ -19,16 +21,67 @@ public class PersonalDataController {
 	@Autowired
 	private UserUtils userUtils;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping("/personal-data")
 	public String getPersonalData(Model model, PersonalDataViewModel personalDataViewModel, Principal principal) {
+		loadPersonalDataPage(principal, model, personalDataViewModel);
+		return "views/all/personalDataView";
+	}
+
+	@PostMapping("/personal-data")
+	public String personalDataActions(Model model, Principal principal, PersonalDataViewModel personalDataViewModel,
+									  @RequestParam(name = "submit", required = false) String actionType,
+									  @RequestParam(name = "currentPassword", required = false) String currentPassword,
+									  @RequestParam(name = "newPassword", required = false) String newPassword,
+									  @RequestParam(name = "personalDataInput", required = false) String personalDataInput) {
 		UserEntity user = userUtils.getUserByEmail(principal.getName());
-		personalDataViewModel.setUserDTO(userUtils.convertFromUserToUserDTO(user));
-		model.addAttribute("personalDataViewModel", personalDataViewModel);
+		
+		switch(actionType) {
+			case "Schimba parola":{
+				changePasswordForUser(user, currentPassword, newPassword, model);
+				loadPersonalDataPage(principal, model, personalDataViewModel);
+				break;
+			}
+			case "Salveaza":{
+				System.out.println(personalDataInput);
+				loadPersonalDataPage(principal, model, personalDataViewModel);
+				break;
+			}
+			case "Anuleaza":{
+				loadPersonalDataPage(principal, model, personalDataViewModel);
+				break;
+			}
+		}
 		return "views/all/personalDataView";
 	}
 	
-	@PostMapping("/personal-data")
-	public String personalDataActions(Model model) {
-		return "views/all/personalDataView";
+	private void loadPersonalDataPage(Principal principal, Model model, PersonalDataViewModel personalDataViewModel) {
+		UserEntity user = userUtils.getUserByEmail(principal.getName());
+		personalDataViewModel.setUserDTO(userUtils.convertFromUserToUserDTO(user));
+		model.addAttribute("personalDataViewModel", personalDataViewModel);	
+	}
+	
+	private void changePasswordForUser(UserEntity user, String currentPassword, String newPassword, Model model) {
+		if(isCurrentPasswordCorect(currentPassword, user) && RegisterValidator.isPasswordValid(newPassword)) {
+			user.setPassword(newPassword);
+			userRepository.save(user);
+			model.addAttribute("passwordChangedSuccessfully", true);
+			return;
+		}
+		if(!isCurrentPasswordCorect(currentPassword, user)) {
+			model.addAttribute("wrongCurrentPassword", true);
+		}
+		if(!RegisterValidator.isPasswordValid(newPassword)) {
+			model.addAttribute("wrongNewPassword", true);
+		}
+	}
+	
+	private boolean isCurrentPasswordCorect(String currentPassword, UserEntity user) {
+		if(!user.getPassword().equals(currentPassword)) {
+			return false;
+		}
+		return true;
 	}
 }
--- tripApp/src/main/java/app/validators/RegisterValidator.java
+++ tripApp/src/main/java/app/validators/RegisterValidator.java
@@ -5,14 +5,14 @@ import java.time.format.DateTimeFormatter;
 
 public class RegisterValidator {
 		
-	public boolean isNameValid(String name) {
-		if(name.length() < 2 || !this.isWellFormated(name) || this.calculateNumberOfConsecutiveSpaces(name) > 1) {
+	public static boolean isNameValid(String name) {
+		if(name.length() < 2 || !isWellFormated(name) || calculateNumberOfConsecutiveSpaces(name) > 1) {
 			return false;
 		}
 		return true;
 	}
 	
-	private Integer calculateNumberOfConsecutiveSpaces(String name) {
+	private static Integer calculateNumberOfConsecutiveSpaces(String name) {
 		Integer numberOfSpaces = 0;
 		for(int i = 0; i < name.length() - 1; i++) {
 			if(name.charAt(i) == ' ' && name.charAt(i + 1) == ' ') {
@@ -22,7 +22,7 @@ public class RegisterValidator {
 		return numberOfSpaces + 1;
 	}
 	
-	private boolean isWellFormated(String text) {
+	private static boolean isWellFormated(String text) {
 		for(int i = 0; i < text.length(); i++) {
 			if(!Character.isLetter(text.charAt(i)) && text.charAt(i) != '-' && text.charAt(i) != ' ') {
 				return false;
@@ -34,7 +34,7 @@ public class RegisterValidator {
 		return true;
 	}
 	
-	public String formatNameProperly(String name) {
+	public static String formatNameProperly(String name) {
 		String allLowerCase = name.toLowerCase();
 		// transform first letter to upper case 
 		allLowerCase = allLowerCase.replace(allLowerCase.charAt(0), Character.toUpperCase(allLowerCase.charAt(0)));
@@ -46,7 +46,7 @@ public class RegisterValidator {
 		return allLowerCase.trim();
 	}
 	
-	private boolean containsCapitalLetter(String text) {
+	private static boolean containsCapitalLetter(String text) {
 		for(int i = 0; i < text.length(); i++) {
 			if(!text.equals(text.toLowerCase())) {
 				return true;
@@ -55,21 +55,21 @@ public class RegisterValidator {
 		return false;
 	}
 	
-	public boolean isPasswordValid(String password) {
-		if(password.length() < 6 || !this.containsCapitalLetter(password)) {
+	public static boolean isPasswordValid(String password) {
+		if(password.length() < 6 || !containsCapitalLetter(password)) {
 			return false;
 		}
 		return true;
 	}
 	
-	public boolean isEmailValid(String email) {
-		if(email.length() < 10 || !this.containsAt(email)) {
+	public static boolean isEmailValid(String email) {
+		if(email.length() < 10 || !containsAt(email)) {
 			return false;
 		}
 		return true;
 	}
 	
-	private boolean containsAt(String text) {
+	private static boolean containsAt(String text) {
 		for(int i = 0; i < text.length(); i++) {
 			// @ cannot be the first letter of email
 			if(text.charAt(i) == '@' && i != 0) {
@@ -79,7 +79,7 @@ public class RegisterValidator {
 		return false;
 	}
 	
-	public boolean isDateValid(String date) {
+	public static boolean isDateValid(String date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate now = LocalDate.now();
 		LocalDate givenDate = LocalDate.parse(date, formatter);
